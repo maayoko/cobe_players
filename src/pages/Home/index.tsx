b02f615aa602cce3d51cf18e3ebbebe3ceeba48e
Ivan <maayoko@gmail.com>
2022-04-26 15:40:42 +0200

    feat(dashboard): sort players alphabetically and reset them

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,18 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 import { NavLink } from "react-router-dom";
 import { usePlayers } from "../../modules/player/usePlayers";
 import { Player } from "../New";
 
 const HomePage = () => {
+    const [indexPlayers, setIndexPlayer] = useState(10);
+    const [sortPlayers, updateSortPlayers] = useState(false);
     const players: Player[] = usePlayers();
-    const handleLoadMorePlayers = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
-        console.log(e)
+    
+    const handleLoadMorePlayers = () => { setIndexPlayer(indexPlayers + 6); }
+    const handleSortPlayers = () => { updateSortPlayers(true); }
+    const handleResetPlayers = () => { updateSortPlayers(false); }
+
+    const initialPlayersToDisplay: Player[] = players.slice(0, indexPlayers);
+    let playersToDisplay: Player[] = [...initialPlayersToDisplay];
+    
+    if (sortPlayers) {
+        playersToDisplay = [...playersToDisplay].sort((a,b) => (a.name > b.name) ? 1 : ((b.name > a.name) ? -1 : 0))
     }
 
     return (
         <div>
+            <div>
+                <button onClick={handleSortPlayers}>Sort players alphabetically</button>
+                <button onClick={handleResetPlayers}>Reset players</button>
+            </div>
             <div style={{ display: "flex", flexWrap: "wrap" }}>
-                {players.map((player, idx) => (
+                {playersToDisplay.map((player, idx) => (
                     <NavLink to={`/player/${player.nickname}`} key={idx} style={{ flexBasis: "33.33333%" }}>
                         <p>
                             <img src={player.photoURL} alt="Player Photo" />
